e3879cc16e0ea3b3b581a378319bc2da89cbcd3b
wscnd <72019998+wscnd@users.noreply.github.com>
2024-02-14 15:58:27 -0300

    exercise 02

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,27 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { getPostsListItems } from "../../models/post.server";
+
+export async function loader() {
+  const posts = await getPostsListItems();
+  return json({ posts });
+}
+
 export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
